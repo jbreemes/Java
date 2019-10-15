2beb023ab1c8455c02d55e09af4f7ec14eb19884
jbreemes <54798893+jbreemes@users.noreply.github.com>
2019-10-15 09:25:53 +0200

    Update

--- src/H13/opdracht131.java
+++ src/H13/opdracht131.java
@@ -1,114 +1,20 @@
 package H13;
 import java.awt.*;
-import java.awt.event.*;
 import java.applet.*;
 
+
 public class opdracht131 extends Applet {
-    Button rood;
-    Button paars;
-    Button grijs;
-    Button blauw;
-    Button groen;
-
-    boolean geklikt = false;
-
-    Color kleurtje;
-    String tekst;
-
-
-    public void init() {
-        rood = new Button("Rood");
-        KnopListener kl = new KnopListener();
-        rood.addActionListener(kl);
-
-        paars = new Button("Paars");
-        KnopListener2 kl2 = new KnopListener2();
-        paars.addActionListener(kl2);
-
-        grijs = new Button("Grijs");
-        KnopListener3 kl3 = new KnopListener3();
-        grijs.addActionListener(kl3);
-
-        blauw = new Button("Blauw");
-        KnopListener4 kl4 = new KnopListener4();
-        blauw.addActionListener(kl4);
-
-        groen = new Button("Groen");
-        KnopListener5 kl5 = new KnopListener5();
-        groen.addActionListener(kl5);
-
-        add(rood);
-        add(paars);
-        add(grijs);
-        add(blauw);
-        add(groen);
-
-    }
-
-    @Override
 
     public void paint(Graphics g) {
 
-        if (geklikt) {
-            achtergrondkleur(g, tekst, kleurtje);
-        }
-
+        driehoek(g,100,200,200,460,400,300);
     }
 
-    void achtergrondkleur(Graphics g, String tekst, Color kleurtje) {
+    void driehoek ( Graphics g, int x, int y, int xx, int yy, int xxx, int yyy) {
 
-        g.drawString("" + tekst, 50, 50);
-        setBackground(kleurtje);
+        g.drawLine(x,y,xx,yy);
+        g.drawLine(xx,yy,xxx,yyy);
+        g.drawLine(xxx,yyy,x,y);
 
-    }
-
-    class KnopListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            tekst = "rood";
-            geklikt = true;
-            kleurtje = Color.RED;
-
-            repaint();
-        }
-    }
-
-    class KnopListener2 implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            tekst = "paars";
-            geklikt = true;
-            kleurtje = Color.MAGENTA;
-
-            repaint();
-        }
-    }
-
-    class KnopListener3 implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            tekst = "grijs";
-            geklikt = true;
-            kleurtje = Color.GRAY;
-
-            repaint();
-        }
-    }
-
-    class KnopListener4 implements  ActionListener {
-        public void  actionPerformed(ActionEvent e) {
-            tekst = "blauw";
-            geklikt = true;
-            kleurtje = Color.BLUE;
-
-            repaint();
-        }
-    }
-
-    class KnopListener5 implements  ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            tekst = "groen";
-            geklikt = true;
-            kleurtje = Color.GREEN;
-
-            repaint();
-        }
     }
 }
